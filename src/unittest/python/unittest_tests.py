a10b05b4d226ab87eba6456e3c1526aedfc46c33
swa3224 <swarnachattopadhyay@gmail.com>
2024-03-10 21:42:45 +0530

    Updated expected result

--- src/unittest/python/unittest_tests.py
+++ src/unittest/python/unittest_tests.py
@@ -11,7 +11,7 @@ class AppTestCase(unittest.TestCase):
     def test_hello_world(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), 'Hello, World!')
+        self.assertEqual(response.data.decode('utf-8'), 'Hi, World!')
 
 if __name__ == '__main__':
     unittest.main()
